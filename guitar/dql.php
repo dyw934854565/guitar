78eed74b79c59d60533f5ea46f1959eb216ecc59
duanyuwen <dyw934854565@sina.com>
2015-08-30 08:40:05 +0800

    *

--- guitar/dql.php
+++ guitar/dql.php
@@ -4,10 +4,37 @@
 	$type = $_POST['type'];
 	$text = $_POST['text'];
 
+	$mysql = new Mysql();
 	if($type == 'pu_search') {
-		$sql = "select pu_list.id,pu_list.pu_name,pu_list.pu_up,pu_list.dowm,pu_pic_list.pu_path from pu_list,pu_pic_list where pu_list.id=pu_pic_list.pu_id and pu_name like '%$text%'";
-		$res = array();
-		$res['sql'] = $sql;
-		echo json_encode($res);
+		$sql = "select pu_list.id as pu_id,pu_list.pu_name,pu_list.pu_up,pu_list.pu_down,pu_pic_list.id as pic_id,pu_pic_list.pu_path from pu_list,pu_pic_list where pu_list.id=pu_pic_list.pu_id and pu_name like '%$text%'";
+		$response = array();
+		$res = $mysql->execute_dql($sql);
+		while ($row = mysql_fetch_assoc($res)) {
+			// array_push($response,$row);
+			// continue;
+			$length = count($response);
+			$has = false;
+			for($i=0; $i<$length; $i++) {
+				if($response[$i]['id'] == $row['pu_id']) {
+					$pic['id'] = $row['pic_id'];
+					$pic['src'] = substr($row['pu_path'], 8);
+					array_push($response[$i]['picArr'],$pic);
+					$has = true;
+				}
+			}
+			if(!$has) {
+				$arow['id'] = $row['pu_id'];
+				$arow['pu_name'] = $row['pu_name'];
+				$arow['pu_up'] = $row['pu_up'];
+				$arow['pu_down'] = $row['pu_down'];
+				$pic['id'] = $row['pic_id'];
+				$pic['src'] = substr($row['pu_path'], 7);
+				$picArr = array();
+				array_push($picArr,$pic);
+				$arow['picArr'] = $picArr;
+				array_push($response,$arow);
+			}
+		}
+		echo json_encode($response);
 	}
 ?>
--- guitar/js/index.js
+++ guitar/js/index.js
@@ -8,9 +8,19 @@ $(function() {
 				type: 'POST',
 				data: {type:'pu_search',text:text},
 				success: function(data) {
-					alert(data);
+					var $ul = $('.search_res_ul').empty();
+					$.each(data, function(i, item) {
+						var li = "<li class='pu_li'><p>"+item['pu_name']+"</p>";
+						li += "<div class='pic_wrap'>";
+						$.each(item.picArr, function(k, pic) {
+							li += "<img src='http://7xlehn.com1.z0.glb.clouddn.com"+pic.src+"-n' />";
+						});
+						li += "</div>";
+						li += "</li>";
+						$ul.append(li);
+					});
 				},
-  				dataType: 'text'
+  				dataType: 'json'
 			});
 		}
 	}
